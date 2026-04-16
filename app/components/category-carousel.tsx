6c4174b2860751203a496326aacbb02493804402
IsaqueLopes-Dev <isaquelopespires@gmail.com>
2026-04-16 10:44:13 -0300

    Update category-carousel.tsx

--- app/components/category-carousel.tsx
+++ app/components/category-carousel.tsx
@@ -2,8 +2,8 @@
 
 import { Suspense, useEffect, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import CategoryCarousel from '../../components/category-carousel';
-import BottomNavigation from '../../components/bottom-navigation';
+import CategoryCarousel from '@/app/components/category-carousel';
+import BottomNavigation from '@/app/components/bottom-navigation';
 
 // --- CONFIGURAÇÕES E TIPOS ---
 const META_PREFIX = '__meta__:';
